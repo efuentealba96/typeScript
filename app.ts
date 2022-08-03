c6a4b99f27adb1b30f01ecb6db16d5bb6e69bfec
efuentealba96 <efuentealba038@gmail.com>
2022-08-03 18:44:07 -0400

    comentario de ayuda agregados

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
+/*tsc -> para transpilar todos los archivos de ts a js*/ 
+/*tsc -w -> para activar el watcher, activamos la transpilacion automatica 
+al hacer el guardado de un archivo ts*/
 const msg : string ='Hola mundo!!';
 const hero = {
     name : 'Ironman',
