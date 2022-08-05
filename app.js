fc04c279f5b4dc6842c999a0b199889f8f5fbcfd
efuentealba96 <efuentealba038@gmail.com>
2022-08-05 13:25:12 -0400

    initial commint in tiposDeDatos

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+// Es idoneo usar el tipo de dato al que corresponde cada variable.
+const a = 10;
+let b;
+function sayHello(msg) {
+    console.log(msg);
+}
---
+++ app.ts
@@ -0,0 +1,7 @@
+// Es idoneo usar el tipo de dato al que corresponde cada variable.
+const a: number = 10;
+let b: string;
+
+function sayHello(msg: string) {
+  console.log(msg);
+}
