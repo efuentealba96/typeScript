e5134d90c6f7206217afac38be2eaf2bcc9afcd4
efuentealba96 <efuentealba038@gmail.com>
2022-08-08 15:12:04 -0400

    add new items

---
+++ number.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    let avengers = 10;
+    console.log(avengers);
+    const villians = 20;
+    if (avengers < villians) {
+        console.log("estamos en problemas");
+    }
+    else {
+        console.log("nos salvamos");
+    }
+})();
